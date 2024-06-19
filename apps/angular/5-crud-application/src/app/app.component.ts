53b4e83f52b2c430d6d339b23ddb9dc375e7277b
Roicel Laya <roicel.laya@gmail.com>
2024-06-19 17:03:25 -0400

    refactor: improving applying best practices

--- apps/angular/5-crud-application/src/app/app.component.ts
+++ apps/angular/5-crud-application/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import {
   Component,
+  DestroyRef,
   inject,
   OnInit,
   signal,
   WritableSignal,
 } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { tap } from 'rxjs/operators';
 import { ITodo } from './models/todo';
 import { TodoService } from './services/todo.service';
 
@@ -24,19 +27,34 @@ import { TodoService } from './services/todo.service';
 export class AppComponent implements OnInit {
   todos: WritableSignal<ITodo[]> = signal([]);
   private readonly todoService = inject(TodoService);
+  private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    this.todoService.getTodos().subscribe((todos) => {
-      this.todos.update(() => [...todos]);
-    });
+    this.todoService
+      .getTodos()
+      .pipe(
+        tap((todos) => this.todos.update(() => [...todos])),
+        takeUntilDestroyed(this.destroyRef),
+      )
+      .subscribe();
   }
 
   update(todo: ITodo) {
-    this.todoService.updateTodo(todo).subscribe((todoUpdated: ITodo) => {
-      this.todos.update(() => [
-        ...this.todos().filter((t) => t.id !== todoUpdated.id),
-        todoUpdated,
-      ]);
-    });
+    this.todoService
+      .updateTodo(todo)
+      .pipe(
+        tap((todoUpdated) => {
+          this.todos.update(() =>
+            this.todos().map((todo) => {
+              if (todo.id !== todoUpdated.id) {
+                return todo;
+              }
+              return todoUpdated;
+            }),
+          );
+        }),
+        takeUntilDestroyed(this.destroyRef),
+      )
+      .subscribe();
   }
 }
